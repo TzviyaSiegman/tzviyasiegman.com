f8f4b79a6dc29b9986034d33d506d9153d731754
BigBlueHat <byoung@bigbluehat.com>
2024-04-10 19:46:59 +0000

    Setup 11ty with basic template using MCSS

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
+    return {
+      dir: {
+        input: "src/",
+
+        // relative to `input` above
+        layouts: "_layouts"
+      }
+    }
+  };
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+title: Tzviya Siegman
+author: Tzviya Siegman
+---
+
+# Welcome
+
+Just getting started.
